5adbbe31127407477e5896b8d1881988c4269487
Vibhanshu Bhardwaj <bhardwajvibhanshu@gmail.com>
2017-02-09 17:14:42 -0500

    too slow even after multiprocessing. also need to work on premature fitness convergence

--- Evolution/Art/pointEvolution.py
+++ Evolution/Art/pointEvolution.py
@@ -1,141 +1,200 @@
-import sys
 import random
-
 import numpy
+
+from copy import deepcopy
 
 from PIL import Image, ImageDraw
 
-try:
-	POPULATION_PER_GENERATION = int(raw_input("Enter population size (preferably between 50 and 75): "))
-except ValueError:
-	print "Didn't enter a number. Choosing 50 as the population size"
-	POPULATION_PER_GENERATION = 50
+import multiprocessing
+
+
+POPULATION_PER_GENERATION = 700
+MUTATION_CHANCE = 0.01
+N_GENES = 500
 
 try:
-	MUTATION_CHANCE = float(raw_input("Enter mutation probability (a number between 1 and 5 ): "))/100
-except ValueError:
-	print "Didn't enter a number. Choosing 1% mutation rate"
-	MUTATION_CHANCE = 0.01
+    TARGET = Image.open("target.png")
+except IOError:
+    print "target.png must be present in current directory. Exiting"
+    exit()
 
-TARGET = None
+
 class Colour:
 
-	def __init__(self, red_val, green_val, blue_val):
-		self.red_val = red_val
-		self.green_val = green_val
-		self.blue_val = blue_val
+    def __init__(self, red_val, green_val, blue_val):
+        self.red_val = red_val
+        self.green_val = green_val
+        self.blue_val = blue_val
+
 
 class Gene:
 
-	def __init__(self, max_size):
-		self.max_size = max_size
-		self.radius = random.uniform(1, 3)
-		self.coordinates = [random.randint(0, max_size[0]), random.randint(0, max_size[1])]
-		self.colour = Colour(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.radius = random.uniform(2, 8)
 
-	def mutate(self):
-		mutation_type = random.choice(["radius", "coordinates", "colour"])
-		magnitude = random.random()
-		if mutation_type == "coordinates":
-			new_X = int(self.coordinates[0] * random.choice([(1+magnitude), (1-magnitude)]))
-			new_Y = int(self.coordinates[1] *  random.choice([(1+magnitude), (1-magnitude)]))
+        self.coordinates = [random.randint(0, max_size[0]),
+                            random.randint(0, max_size[1])]
 
-			self.coordinates = [new_X, new_Y]
+        self.colour = Colour(random.randint(0, 255),
+                             random.randint(0, 255),
+                             random.randint(0, 255))
 
-		elif mutation_type == "radius":
-			self.radius = self.radius * random.choice([(1+magnitude), (1-magnitude)])
+    def mutate(self):
+        mutation_type = random.choice(["radius", "coordinates", "colour"])
+        magnitude = max(1, int(round(random.gauss(15, 4))))/100
 
-		else:
-			new_red_val = min(self.colour.red_val * (random.choice([(1+magnitude), (1-magnitude)])), 255)
-			new_green_val = min(self.colour.green_val * (random.choice([(1+magnitude), (1-magnitude)])), 255)
-			new_blue_val = min(self.colour.blue_val * (random.choice([(1+magnitude), (1-magnitude)])), 255)
+        if mutation_type == "coordinates":
+            new_X = max(0, random.randint(
+                                    int(self.coordinates[0]*(1-magnitude)),
+                                    int(self.coordinates[0]*(1+magnitude))))
+            new_Y = max(0, random.randint(
+                                    int(self.coordinates[1]*(1-magnitude)),
+                                    int(self.coordinates[1]*(1+magnitude))))
 
-			self.colour = Colour(new_red_val, new_green_val, new_blue_val)
+            self.coordinates = [new_X, new_Y]
+
+        elif mutation_type == "radius":
+            self.radius = max(1, random.randint(
+                                    int(self.radius*(1-magnitude)),
+                                    int(self.radius*(1+magnitude))))
+
+        else:
+            new_red_val = min(max(0, random.randint(
+                                    int(self.colour.red_val*(1-magnitude)),
+                                    int(self.colour.red_val*(1+magnitude)))),
+                              255)
+
+            new_green_val = min(max(0, random.randint(
+                                    int(self.colour.green_val*(1-magnitude)),
+                                    int(self.colour.green_val*(1+magnitude)))),
+                                255)
+            new_blue_val = min(max(0, random.randint(
+                                    int(self.colour.blue_val*(1-magnitude)),
+                                    int(self.colour.blue_val*(1+magnitude)))),
+                               255)
+
+            self.colour = Colour(new_red_val, new_green_val, new_blue_val)
 
 
 class Organism:
 
-	def __init__(self, size, n_genes):
-		self.size = size
-		self.genes = [Gene(size) for i in xrange(max(n_genes, 400))] #no more than 400 genes
-		self.fitness = 0
+    def __init__(self, size):
+        self.size = size
+        self.genes = [Gene(size) for i in xrange(N_GENES)]
 
-	def mutate(self):
-		for gene in self.genes:
-			if random.random() < MUTATION_CHANCE:
-				gene.mutate()
+    def mutate(self):
+        for gene in self.genes:
+            if random.random() < MUTATION_CHANCE:
+                gene.mutate()
 
-	def crossover(self, partner):
+    def crossover(self, partner):
 
-		offspring = Gene(len(self.genes))
-		mid = len(this.genes)/2
+        offspring = Organism(self.size, len(self.genes))
 
-		for i in xrange(0, mid):
-			offspring.genes[i] = self.genes[i]
+        for i in xrange(0, len(self.genes)):
+            if random.random() < 0.5:
+                offspring.genes[i] = self.genes[i]
+            else:
+                offspring.genes[i] = partner.genes[i]
 
-		for j in xrange(mid, len(self.genes)):
-			offspring.genes[j] = self.genes[j]
+        return offspring
 
-		return offspring
+    def draw(self):
 
-	def draw(self):
-		image = Image.new("RGB",self.size,(255,255,255))
-		canvas = ImageDraw.Draw(image)
+        image = Image.new("RGB", self.size, (255, 255, 255))
+        canvas = ImageDraw.Draw(image)
 
-		for gene in self.genes:
-			colour = (gene.colour.red_val,gene.colour.green_val,gene.colour.blue_val)
-			canvas.ellipse([gene.coordinates[0]-(2*gene.radius),
-							gene.coordinates[1]-(2*gene.radius),
-							gene.coordinates[0]+(2*gene.radius),
-							gene.coordinates[1]+(2*gene.radius)],
-							outline=colour,fill=colour)
+        for gene in self.genes:
+            colour = (int(gene.colour.red_val),
+                      int(gene.colour.green_val),
+                      int(gene.colour.blue_val))
+
+            canvas.ellipse([gene.coordinates[0]-(2*gene.radius),
+                            gene.coordinates[1]-(2*gene.radius),
+                            gene.coordinates[0]+(2*gene.radius),
+                            gene.coordinates[1]+(2*gene.radius)],
+                           outline=colour, fill=colour)
 
         return image
 
-	def set_fitness(self, target):
-		image = draw()
+    def set_fitness(self, target):
+        new = deepcopy(self)
+        image = new.draw()
 
-		raw_self = numpy.array(image, numpy.int16)
-		raw_target = numpy.array(target, numpy.int16)
+        raw_self = numpy.array(image, numpy.int16)
+        raw_target = numpy.array(target, numpy.int16)
 
-		delta = float(numpy.sum(numpy.abs(raw_self - raw_target)))
+        delta = float(numpy.sum(numpy.abs(raw_self - raw_target)))
+        self.fitness = (delta / 255.0 * 100) / raw_self.size
 
-		self.fitness = ((delta/255) * 100) / raw_self.size
+
+def FITNESS(organism):
+        image = organism.draw()
+
+        raw_self = numpy.array(image, numpy.int16)
+        raw_target = numpy.array(TARGET, numpy.int16)
+
+        delta = float(numpy.sum(numpy.abs(raw_self - raw_target)))
+        return (delta / 255.0 * 100) / raw_self.size
+
 
 class Population:
 
-	def __init__(self, target_size):
-		self.population = [Organism(target_size, N_GENES) for i in xrange(POPULATION_PER_GENERATION)]
-		self.mating_pool = []
+    def __init__(self, target):
+        self.population = [Organism(target.size, N_GENES) for i in xrange(POPULATION_PER_GENERATION)]
+        self.mating_pool = []
+        self.generations = 0
+        self.max_fitness = 0
 
-		#for organism in self.population:
+    def set_fitness(self, target):
+        multi = multiprocessing.Pool(multiprocessing.cpu_count())
+        results = multi.map(FITNESS, self.population)
+        for i in xrange(len(self.population)):
+            self.population[i].fitness = results[i]
 
-	def set_fitness(self):
+    def generate(self, target):
+        sorted_population = sorted(self.population, key=lambda (o): o.fitness)[:200]
+        for i in xrange(len(self.population)):
+            parent_A = random.choice(sorted_population)
+            parent_B = random.choice(sorted_population)
 
-		for organism in self.population: #maybe this goes in another method.
-			organism.set_fitness(TARGET)
+            offspring = parent_A.crossover(parent_B)
+            offspring.mutate()
+            offspring.set_fitness(target)
 
+            self.population[i] = offspring
+        self.generations += 1
 
-	def natural_selection(self):
-		self.max_fitness = 0
-		self.mating_pool = []
-		for organism in self.population:
-			max_fitness = max(organism.fitness, max_fitness)
+    def best_of_generation(self):
+        curr_best = self.population[0]
+        for organism in self.population:
+            if organism.fitness < curr_best.fitness:
+                curr_best = organism
+        return curr_best
 
-		for i in xrange(len(self.population)):
-			n = int(self.population[i].fitness * 10)
-			for j in xrange(n):
-				self.mating_pool.append(self.population[i])
-
-	def generate(self):
-		for i in xrange(len(self.population)):
-			parent_A = random.choice(self.population)
-			parent_B = random.choice(self.population)
-
-			offspring = parent_A.crossover(parent_B)
-			offspring.mutate()
-
-			self.population[i] = offspring
-
+    def average_fitness(self):
+        total_fitness = 0
+        for organism in self.population:
+            total_fitness += organism.fitness
+        return total_fitness/len(self.population)
 
 
+def start(target):
+    population = Population(target)
+    population.set_fitness(target)
+
+    i = 0
+    while i < 4000:
+        print i
+        population.generate(target)
+
+        if i % 10 == 0:
+            print "saved " + str(i)
+            population.best_of_generation().draw().save(str(i)+".png")
+        i += 1
+
+
+if __name__ == "__main__":
+    print "Starting up. Sit back- this will take a while."
+    start(TARGET)
